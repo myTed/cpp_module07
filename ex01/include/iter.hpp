8fa8997431744dc0592b11b7cd219d7a13b71759
myTed <tribune11@nate.com>
2023-01-25 19:38:26 +0900

    ex01 for const object 템플릿 제거

--- ex01/include/iter.hpp
+++ ex01/include/iter.hpp
@@ -3,29 +3,10 @@
 
 #include <iostream>
 
-//for const object
-template <typename T>
-void	func(const T& rf)
-{
-	std::cout << rf << std::endl;
-}
-
 template <typename T>
 void	func(T& rf)
 {
 	std::cout << rf << std::endl;
-}
-
-//for const object
-template <typename T>
-void	iter(const T* const startAddr, int length, void (*f)(const T& rT))
-{
-	if (startAddr == 0)
-		return ;
-	for (int idx = 0; idx < length; idx++)
-	{
-		f(startAddr[idx]);
-	}
 }
 
 template <typename T>
--- ex01/main.cpp
+++ ex01/main.cpp
@@ -2,13 +2,13 @@
 
 int	main(void)
 {
-	std::string arry[3] = {"aaaaa", "bbbb", "cccc"};
+	const std::string arry[3] = {"aaaaa", "bbbb", "cccc"};
 	iter(arry, 3 , func);
 	
 	std::cout << arry[0] << std::endl;
 	std::cout << arry[1] << std::endl;
 	std::cout << arry[2] << std::endl;
-	
+
 	char	arry2[3] = {'a', 'b', 'c'};
 	iter(arry2, sizeof(arry) / sizeof(arry[0]), func);
 

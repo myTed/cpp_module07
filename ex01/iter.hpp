82c504078498021d3e90eb179261d13f8e7c63a3
myTed <tribune11@nate.com>
2023-01-27 04:34:24 +0900

    평가 후 수정

--- ex01/iter.hpp
+++ ex01/iter.hpp
@@ -4,9 +4,26 @@
 #include <iostream>
 
 template <typename T>
+void	func(const T& rf)
+{
+	std::cout << rf << std::endl;
+}
+
+template <typename T>
 void	func(T& rf)
 {
 	std::cout << rf << std::endl;
+}
+
+template <typename T>
+void	iter(T* const startAddr, int length, void (*f)(const T& rT))
+{
+	if (startAddr == 0)
+		return ;
+	for (int idx = 0; idx < length; idx++)
+	{
+		f(startAddr[idx]);
+	}
 }
 
 template <typename T>

efd366b589c1a36c928f209ea4b47232c7d4b748
myTed <tribune11@nate.com>
2023-01-26 20:12:11 +0900

    평가전 수정

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -15,6 +15,9 @@ int	main(void)
 	int		arry3[3] = {1,2,3};
 	iter(arry3, sizeof(arry3) / sizeof(arry3[0]), func);
 
+	const int arry4[3] = {7, 8, 9};
+	iter(arry4, sizeof(arry4) / sizeof(arry4[0]), func);
+
 	iter(arry, -1, func);
 	
 	return (0);
